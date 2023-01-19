54ff44476e17614b7258b1b386ff712354dc249c
jitain1 <singhjitainj@gmail.com>
2023-01-20 02:28:54 +0530

    Added: Creating a new LinkList

--- src/com/bridgelabz/LinkedList/LinkListOperations.java
+++ src/com/bridgelabz/LinkedList/LinkListOperations.java
@@ -3,7 +3,8 @@ package com.bridgelabz.LinkedList;
 public class LinkListOperations {
 
 	public static void main(String[] args) {
-		
+		LinkedList<Integer> Linkedlist1 = new LinkedList<>();
+		Linkedlist1.push(70);
 
 	}
 
